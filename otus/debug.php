9951a6f2bdc209aaa864c0899bce3705138660ac
Kutukov Danil <dka0000@yandex.ru>
2025-06-27 23:01:21 +0300

    DZ_2 - Debugging and logging (hotfix)

--- otus/debug.php
+++ otus/debug.php
@@ -2,5 +2,5 @@
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новая страница");
-
+$gggg = 5/0;
 Bitrix\Main\Diag\Debug::dumpToFile(date('Y-m-d H:i:s'),"Время открытия странички edu.kiberone-rostov.ru/otus/debug.php");
